ec4323e0e40582c309cadf11a3fded46e7ba8d9b
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-05-16 01:57:26 +0530

    feat: added functionality for custom short url

--- API/routes/shorten.go
+++ API/routes/shorten.go
@@ -55,5 +55,36 @@ func ShortenUrl(c *fiber.Ctx) error{
    }
 
    body.URL = helpers.EnforceHTTP(body.URL)
+
+   var id string
+
+   if body.CustomShort == ""{
+      id = uuid.New().String()[:6]
+   }else{
+      id = body.CustomShort
+   }
+   
+   r := database.CreateClient(0)
+   defer r.Close()
+
+   val, _ = r.Get(database.Ctx, id).Result()
+
+   if val != ""{
+      return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+         "error":"URL custom short url is already in use",
+      })
+   }
+
+   if body.Expiry == 0{
+      body.expiry = 24
+   }
+
+   err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+   if err != nil {
+      return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+        "error":"unable to connect to the server",
+      })
+   }
+
    r2.Decr(database.Ctx, c.IP())
 }
